1d013854f3f17722c87ab944d68bd6c1b5f041e3
codeproject-th <70337595+codeproject-th@users.noreply.github.com>
2021-01-22 22:25:11 +0700

    Delete host directory

--- host/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-// database
-define('MYSQL_HOST','localhost');
-define('MYSQL_USERNAME','webappth_b17');
-define('MYSQL_PASSWORD','vd7grRDa');
-define('MYSQL_DATABASE','webappth_b17');
-define('BASE_URL','');
-?>
